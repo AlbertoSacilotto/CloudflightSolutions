18d68f3c22b64d2ed2f87a6dc244712b0c121bf4
coderalberto <48130185+AlbertoSacilotto@users.noreply.github.com>
2023-03-15 15:54:15 +0100

    Ranking System LVL 2
    Working Ranking System LVL 2

---
+++ RankingSystemLVL2/Program.cs
@@ -0,0 +1,42 @@
+﻿using RankingSystem;
+using System;
+using System.IO;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            string filePath = "input.txt"; //put input file in bin archive
+            string fileContent = File.ReadAllText(filePath);
+            string[] lines = fileContent.Split(' ');
+            DateTime start = DateTime.Parse(lines[0]);
+            //int totalSub = Int32.Parse(lines[1]);  total inputs not needed 
+            List<Submission> list = new List<Submission>();
+            for (int i = 2; i < lines.Length; i+=3)
+            {
+                if (lines[i + 2] == "correct")
+                {
+                    list.Add(new Submission(Int32.Parse(lines[i]), DateTime.Parse(lines[i+1]), true));
+                }
+            }
+            Submission res = FastestSub(list, start);
+            Console.WriteLine($"{res.ID} {res.Sub}");
+        }
+        static Submission FastestSub(List<Submission> list, DateTime start)
+        {
+            Submission rtn = list[0];
+            for (int i = 1; i < list.Count; i++)
+            {
+                if ((int)(list[i].Sub - start).TotalSeconds < (int)(rtn.Sub - start).TotalSeconds)
+                {
+                    rtn = list[i];
+                }
+            }
+            return rtn;
+        }
+    }
+}
---
+++ RankingSystemLVL2/Submission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankingSystem
+{
+    public class Submission
+    {
+        public int ID { get; set; }
+        public DateTime Sub { get; set; }
+        public bool Correct { get; set; }
+
+        public Submission(int id, DateTime sub, bool correct)
+        {
+            ID = id;
+            Sub = sub;
+            Correct = correct;
+        }
+
+    }
+}
